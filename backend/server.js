c2dad553b311c60f0c0a62dbf0846305b8a95fc2
dhruvermafz <vermadhruv09112002@gmail.com>
2024-03-15 13:41:59 +0530

    adding client

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://friendzone-nine.vercel.app"],
+    origin: ["http://localhost:5173", "https://friendzone-social.vercel.app"],
     credentials: true,
   })
 );
