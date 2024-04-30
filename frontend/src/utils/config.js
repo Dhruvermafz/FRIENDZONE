2d00b71f37482c52f5dc4e913148fe7b38ff6d82
dhruvermafz <vermadhruv09112002@gmail.com>
2024-04-30 21:25:51 +0530

    config updated nth time

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -2,9 +2,9 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 let REACT_APP_API_URL = "https://friendzone-backend.onrender.com";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  REACT_APP_API_URL = "https://friendzone-backend.onrender.com";
+  REACT_APP_API_URL = "http://localhost:8000";
 }
-const API_URL = "https://friendzone-backend.onrender.com";
+const API_URL = "http://localhost:8000";
 const API_BASE_URL = REACT_APP_API_URL;
 const CLOUDINARY_URL = "https://api.cloudinary.com/v1_1/live-auctions/uploads";
 const UPLOAD_PRESET = "hgvapsg0";
